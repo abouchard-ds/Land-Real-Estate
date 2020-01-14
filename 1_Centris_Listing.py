c590263ff173f47f6853ea305a6219c51486089a
Alexandre Bouchard <38104156+abouchard-ds@users.noreply.github.com>
2020-01-13 22:04:31 -0500

    Update 1_Centris_Listing.py

--- 1_Centris_Listing.py
+++ 1_Centris_Listing.py
@@ -4,7 +4,6 @@
 """
 
 from selenium import webdriver
-from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.firefox.options import Options
 
